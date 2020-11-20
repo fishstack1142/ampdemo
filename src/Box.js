47038240d2e156240392ea76b9d78cf4942c4eba
FishStack1142 <fishstack1142@gmail.com>
2020-11-20 21:23:52 +0700

    use axios

--- src/Box.js
+++ src/Box.js
@@ -1,7 +1,23 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 function Box() {
+    const [name, setName] = useState('');
+
+    useEffect(() => {
+
+        async function fetchData() {
+        const result = await axios('https://swapi.dev/api/people/5/',);
+
+        console.log(result)
+        console.log(result.data.name)
+        setName(result.data.name)
+        }
+        fetchData();
+    }, []);
+
   return (
     <div>
-      <h4>second amp....</h4>
+      <h4>The name is {name}</h4>
     </div>
   );
 }
