818294cffbe7270fc8d2c4f4b99a6e29f3b6d000
FishStack1142 <fishstack1142@gmail.com>
2020-11-20 22:15:54 +0700

    use textbox

--- src/Box.js
+++ src/Box.js
@@ -1,22 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 function Box() {
-    const [name, setName] = useState('');
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("1");
 
-    useEffect(() => {
+  useEffect(() => {
+    fetchData();
+  });
 
-        async function fetchData() {
-        const result = await axios('https://swapi.dev/api/people/5/',);
+  async function fetchData() {
+    const result = await axios(`https://swapi.dev/api/people/${number}/`);
+    setName(result.data.name)
+  }
 
-        console.log(result)
-        console.log(result.data.name)
-        setName(result.data.name)
-        }
-        fetchData();
-    }, []);
+  const handleChange = event => {
+    event.preventDefault();
+      console.log(event)
+      setNumber(event.target.value)
+
+  }
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    fetchData();
+  };
 
   return (
     <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleChange} />
+        <input type="submit" value="Submit" />
+      </form>
       <h4>The name is {name}</h4>
     </div>
   );
